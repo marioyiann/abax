92c1e344c24c3a2b7a86c3549aba699a16759473
Marios Yiannakopoulos <mgiannakopoulos@gmail.com>
2015-12-04 17:30:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Free HTML5 & CSS3 template, based on bootstrap.
+---
+
+Abax is a minimal one page website. The purpose of this template is to give a basic layout, for promoting your products or services. You're allowed to download it for free and customize according to your needs.
+
+The advantage of Abax is:
+- Automated development process based on Gulp
+- Built with Sass for easy customization. 
+
+[Demo](http://marioyiann.com/abax/)
+
+**Features**
+- HTML5/CSS3
+- SASS
+- Responsive layout
+- Bootstrap v3.3.5
+- Gulp 3.9.0
+
+## Getting Started
+
+### 1. Git clone
+```sh
+$ git clone https://github.com/marioyiann/abax.git
+```
+
+### 2. Install node modules 
+Make sure you have installed [Node.js](https://nodejs.org) 
+```sh
+$ npm install
+```
+
+### 4. Run Gulp
+```sh
+$ gulp
+```
+
+### 5. Start editing
+
+Hope you enjoy it and find it useful!
+Find me on: [Twitter](https://twitter.com/marioyiann)
+
+##### Thanks and Respect to all external resources!
+Bootstrap: http://getbootstrap.com/  
+Fontawesome: http://fontawesome.io/  
+Animation: https://github.com/daneden/animate.css
+Wow: https://github.com/matthieua/WOW
+Lightbox2: https://github.com/lokesh/lightbox2/
+Images: https://unsplash.com/ugmonk
---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp'),
+		sass = require ('gulp-sass'),
+		cssmin = require('gulp-cssmin'),
+		rename = require('gulp-rename'),
+		gutil = require('gulp-util'),
+		uglify = require('gulp-uglify'),
+		livereload= require('gulp-livereload'),
+		imagemin = require('gulp-imagemin'),
+		prefix = require('gulp-autoprefixer'),
+		webserver = require('gulp-webserver');
+	
+function onError(err) {
+  console.log(err);
+  this.emit('end');
+}	
+
+// Styles
+gulp.task('sass', function() {
+	gulp.src('dev/scss/**/*.scss')
+		.pipe(sass({outputStyle: 'compressed' }))
+		.on('error', onError)
+		.pipe(prefix())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(livereload());
+});
+
+gulp.task('cssmin', function () {
+    gulp.src('dev/assets/vendors/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('app/css'));
+});
+
+// Scripts
+gulp.task('scripts', function(){
+	gulp.src('dev/js/*.js')
+		.pipe(uglify().on('error', gutil.log))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('app/js/'));
+});
+
+// Image compress
+gulp.task('image', function(){
+	gulp.src('app/img/*')
+		.pipe(imagemin({ progressive: true }))
+		.pipe(gulp.dest('app/img/'));
+});
+
+// Run webserver http://localhost:8000/
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(webserver({
+      livereload: {
+        enable: true, // need this set to true to enable livereload
+        filter: function(fileName) {
+          if (fileName.match(/.map$/)) { // exclude all source maps from livereload
+            return false;
+          } else {
+            return true;
+          }
+        }
+      }
+    }));
+});
+
+// Watch
+gulp.task('watch', function(){
+
+	var server = livereload();
+
+	gulp.watch('dev/scss/**/*.scss', ['sass']);
+	gulp.watch('dev/js/*.js', ['scripts']);
+});
+
+gulp.task('default', ['sass', 'cssmin', 'scripts', 'image', 'webserver', 'watch']);
